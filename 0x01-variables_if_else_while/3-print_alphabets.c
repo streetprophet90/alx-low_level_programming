5af94faba16792698c71c7de7ffcab209987c5b7
streetprophet90 <hamoafo@gmail.com>
2022-12-09 15:25:23 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase and uppercase followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+		putchar ('\n');
+	/* print uppercase letters */
+
+	for (ch = 'A'; ch <= 'Z', ch++)
+		putchar(ch);
+
+		putchar (\n') ;
+		return (0);
+}
