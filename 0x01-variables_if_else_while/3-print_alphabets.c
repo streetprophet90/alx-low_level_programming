52256e017d7a7228a5476c8a203a1a79e3d577a3
streetprophet90 <hamoafo@gmail.com>
2022-12-09 15:34:15 -0800

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,17 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
 
 		putchar ('\n');
 	/* print uppercase letters */
 
 	for (ch = 'A'; ch <= 'Z', ch++)
+	{
 		putchar(ch);
+	}
 
 		putchar (\n') ;
 		return (0);
