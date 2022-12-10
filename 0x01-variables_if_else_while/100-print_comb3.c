89c89ec2c8f118924e71a9ba52f3e63c38451857
streetprophet90 <hamoafo@gmail.com>
2022-12-10 01:05:00 -0800

    print comb 3)

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,9 +13,9 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0' tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0' ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
 			{
