f475028f8f4206e5e19b357cd103b0b3a4fb31cd
streetprophet90 <hamoafo@gmail.com>
2022-12-09 17:51:37 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0' tens <= '0'; tens++) /*print tens digit*/
+	for (tens = '0' tens <= '9'; tens++) /*print tens digit*/
 	{
-		for (ones = '0' ones <= '0'; ones++)/*prints ones digit*/
+		for (ones = '0' ones <= '9'; ones++)/*prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repition*/
 			{
