7c3e758b20fd52369929ce8b8043f79d16273201
streetprophet90 <hamoafo@gmail.com>
2022-12-14 06:30:40 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Description: prints ever minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
+		}
+	}
+}
