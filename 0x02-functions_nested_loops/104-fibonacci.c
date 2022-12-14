2343dffc1e7dda554abcb8feed37ae271a68e557
streetprophet90 <hamoafo@gmail.com>
2022-12-14 09:04:49 -0800

    first 98 fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the first 98 fibonacci numbers, starting with
- * 	1 and 2, separated by a comma followed by a space.
+ * 1 and 2, separated by a comma followed by a space.
  *
  * Return: ALways 0.
  */
