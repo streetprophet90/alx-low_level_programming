ee55581687ce6be1e497087bff16573d79ecc317
streetprophet90 <hamoafo@gmail.com>
2022-12-14 09:56:54 -0800

    fib random

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,52 +1,62 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space.
- *
- * Return: ALways 0.
+ * numlength - returns the length of string
+ * @num : operand number
+ * Return: number of digits
  */
+int numlength(int num)
+{
+	int length = 0
 
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/*
+ *  *main - prints the first 98 fib sequences
+ *  Return: 0
+ */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 10000000000, f10 = 0;
+	unsigned long f20 = 0, tmpo = 0;
+	short int i = 1, initial0s;
 
-	for (count = 0; count < 92; count++)
+	while (i <= 98)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		if (f10 > 0)
+			printf("%lu", f10);
+		initial0s = numlength(mx) - 1 - numlength(f1);
+		while (f10 > 0 && initial0s > 0)
 		{
-			half1 += 1;
-			half2 %= 100000000000;
+			printf("%l", 0);
+			initial0s--;
 		}
+		printf("%lu", f1);
 
-		printf("%lu%lu", half1, half2);
-		if (count != 99)
+		tmp = (f1 + f2) % mx;
+		tmpo = f10 + f20 + (f1 + f2) / mx;
+		f1 = f2;
+		f10 = f20;
+		f2 = tmp;
+		f20 = tmpo;
+
+		if (i != 98)
 			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		else
+			printf("\n");
+		i++;
 	}
-	printf("\n");
 	return (0);
 }
+
