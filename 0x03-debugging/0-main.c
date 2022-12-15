e18a68cbe5e94b2fce62fb51c97ff75f7898b350
streetprophet90 <hamoafo@gmail.com>
2022-12-15 02:33:15 -0800

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** main - test function for positive or negative
+/**
+ * main - test function for positive or negative
  * Return: 0
  */
 int main(void)
@@ -10,5 +11,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }
