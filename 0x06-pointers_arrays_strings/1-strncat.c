e03f57ec763f5090ce3f305a88df03722714fe3d
streetprophet90 <hamoafo@gmail.com>
2022-12-23 18:31:55 -0800

    conc on 2 string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates  two strings
+ * _strncat - conc two strings
+ * @dest: the string to be appended upon
+ * @src: the string to be appended to dest.
+ * @n: The number of bytes from src to be appended
  *
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- *
- * Return: pointer to the resulting string dest
+ * Return: A pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
