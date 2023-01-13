66eb85b8020b9282e7c59310531f2019c329141c
streetprophet90 <hamoafo@gmail.com>
2023-01-13 04:58:29 -0800

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1 && s1[len])
 		len++;
 	while (s2 && s2[len1])
-		len1++
+		len1++;
 
 	if (n < len1)
 		s = malloc((len + n + 1) * sizeof(char));
